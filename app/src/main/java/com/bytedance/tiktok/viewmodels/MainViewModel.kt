3f96aff9a6b6005dc6eda96b27a776b2fa605c31
demoless <1097959060@qq.com>
2020-07-03 20:58:03 +0800

    可见性修改

--- app/src/main/java/com/bytedance/tiktok/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/bytedance/tiktok/viewmodels/MainViewModel.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    public val state : MutableLiveData<Boolean> by lazy {
+    val state : MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>().apply{
             this.value = true
         }
     }
 
-    public val pageChangeEvent : MutableLiveData<Int> by lazy {
+    val pageChangeEvent : MutableLiveData<Int> by lazy {
         MutableLiveData<Int>().apply {
             this.value = 0;
         }
