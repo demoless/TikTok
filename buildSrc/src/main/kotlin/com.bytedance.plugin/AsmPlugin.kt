f6e6709d50b7586bffa979ee969d4fb3af212c8d
zhangfan <zhangfan.demoless@bytedance.com>
2020-09-04 17:25:40 +0800

    Asm Plugin

---
+++ buildSrc/src/main/kotlin/com.bytedance.plugin/AsmPlugin.kt
@@ -0,0 +1,23 @@
+package com.bytedance.plugin
+
+import com.android.build.api.transform.Transform
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AsmPlugin: Plugin<Project>{
+    override fun apply(project: Project) {
+        //注册方式1
+        val appExtension = project.extensions.getByType(AppExtension::class.java)
+        appExtension.registerTransform(getCustomTransform(project))
+        //注册之后会在TransformManager#addTransform中生成一个task.
+
+        //注册方式2
+        //project.android.registerTransform(getCustomTransform())
+    }
+
+    private fun getCustomTransform(project: Project): Transform {
+        return AsmTransform()
+    }
+
+}
--- buildSrc/src/main/kotlin/com.bytedance.plugin/AsmTransform.kt
+++ buildSrc/src/main/kotlin/com.bytedance.plugin/AsmTransform.kt
@@ -4,7 +4,12 @@ import com.android.build.api.transform.*
 import com.android.build.gradle.internal.pipeline.TransformManager
 import com.android.ide.common.internal.WaitableExecutor
 import org.gradle.internal.impldep.org.apache.commons.io.FileUtils
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.ClassWriter
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 class AsmTransform: Transform() {
     companion object {
@@ -75,7 +80,7 @@ class AsmTransform: Transform() {
                     Status.NOTCHANGED -> return
                     Status.CHANGED,Status.ADDED -> {
                         FileUtils.touch(destFile)
-                        FileUtils.copyFile(directory,destFile)
+                        transformSingleFile(inputFile,destFile)
                     }
                     Status.REMOVED -> {
                         if (destFile.exists()) {
@@ -85,7 +90,7 @@ class AsmTransform: Transform() {
                 }
             }
         } else {
-            FileUtils.copyDirectory(directoryInput.file,directory)
+            transformDirectory(directoryInput,directory)
         }
     }
 
@@ -109,6 +114,58 @@ class AsmTransform: Transform() {
             }
         } else {
             FileUtils.copyFile(jarInput.file,dest)
+        }
+    }
+
+    private fun transformSingleFile(inputFile: File, destFile: File) {
+        println("拷贝单个文件")
+        traceFile(inputFile, destFile)
+    }
+
+    private fun traceFile(inputFile:File, outputFile:File) {
+        if (isNeedTraceClass(inputFile)) {
+            println("${inputFile.name} ---- 需要插桩 ----")
+            val inputStream = FileInputStream(inputFile)
+            val outputStream = FileOutputStream(outputFile)
+
+            val classReader = ClassReader(inputStream)
+            val classWriter = ClassWriter(ClassWriter.COMPUTE_MAXS)
+            classReader.accept(getClassVisitor(classWriter), ClassReader.EXPAND_FRAMES)
+            outputStream.write(classWriter.toByteArray())
+
+            inputStream.close()
+            outputStream.close()
+        } else {
+            FileUtils.copyFile(inputFile, outputFile)
+        }
+    }
+
+    private fun isNeedTraceClass(inputFile: File):Boolean {
+        val name = inputFile.name
+        return !(!name.endsWith(".class")
+                || name.startsWith("R.class")
+                || name.startsWith("R$"))
+    }
+
+    private fun getClassVisitor(classWriter:ClassWriter): ClassVisitor {
+        return DoubleClickClassVisitor(classWriter)
+    }
+
+    private fun transformDirectory(directoryInput: DirectoryInput,dest: File) {
+        val extensions = arrayOf("class")
+        //递归地去获取该文件夹下面所有的文件
+        val fileList = FileUtils.listFiles(directoryInput.file,extensions,true)
+        val outputFilePath = dest.absolutePath
+        val inputFilePath = directoryInput.file.absolutePath
+        fileList.forEach { inputFile ->
+            println("替换前  file.absolutePath = ${inputFile.absolutePath}")
+            val outputFullPath = inputFile.absolutePath.replace(inputFilePath, outputFilePath)
+            println("替换后  file.absolutePath = ${outputFullPath}")
+            val outputFile = File(outputFullPath)
+            //创建文件
+            FileUtils.touch(outputFile)
+            //单个单个地复制文件
+            transformSingleFile(inputFile, outputFile)
         }
     }
 }
