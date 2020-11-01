db48aaf537b0ace09ce6fe2211010c131c36a94b
demoless <1097959060@qq.com>
2020-11-01 14:04:26 +0800

    Optimization: 使用RxJava kotlin改造首屏页

--- app/src/main/java/com/bytedance/tiktok/activity/SplashActivity.java
+++ app/src/main/java/com/bytedance/tiktok/activity/SplashActivity.java
@@ -1,10 +1,17 @@
 package com.bytedance.tiktok.activity;
 
 import android.content.Intent;
-import android.os.CountDownTimer;
 
 import com.bytedance.tiktok.R;
 import com.bytedance.tiktok.base.BaseActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 /**
  * create on 2020/5/19
@@ -20,18 +27,22 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void init() {
         setFullScreen();
-
-        new CountDownTimer(500, 500) {
+        Single.timer(500, TimeUnit.MILLISECONDS).subscribeOn(Schedulers.io()).subscribe(new SingleObserver<Long>() {
             @Override
-            public void onTick(long millisUntilFinished) {
+            public void onSubscribe(@NonNull Disposable d) {
 
             }
 
             @Override
-            public void onFinish() {
+            public void onSuccess(@NonNull Long aLong) {
                 startActivity(new Intent(SplashActivity.this, MainKtActivity.class));
                 finish();
             }
-        }.start();
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
