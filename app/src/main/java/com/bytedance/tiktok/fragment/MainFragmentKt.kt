dfcd2ab6173a85bfc5b250f1e533818d0d13a46e
demoless <1097959060@qq.com>
2020-11-05 08:58:47 +0800

    Optimization: grid item布局优化

--- app/src/main/java/com/bytedance/tiktok/fragment/MainFragmentKt.kt
+++ app/src/main/java/com/bytedance/tiktok/fragment/MainFragmentKt.kt
@@ -50,6 +50,7 @@ class MainFragmentKt : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setFragments()
     }
 
